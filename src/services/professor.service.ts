65374113eb4caa1f083d899991fb5de8e9793d6e
Deivyson Ricardo Silva dos Santos <131066052+Deivyson05@users.noreply.github.com>
2026-04-13 22:40:15 +0000

    config: configura esquema de entidades e conexão com banco de dados

---
+++ src/services/professor.service.ts
@@ -0,0 +1,23 @@
+
+import { prisma } from "../../lib/prisma";
+import { HttpError } from "../core/httpError";
+
+export class ProfessorService {
+    static async create(body: any) {
+        //const { name, email, password } = body;
+
+        //if (!name || !email || !password) {
+        //    throw new HttpError("Name, email, and password are required", 400);
+        //}
+
+        //const professor = await prisma.professor.create({
+        //    data: {
+        //        
+        //    },
+        //    include: {
+        //        
+        //    },
+        //});
+    }
+}
+    
