cc275f04e3cb1b972548a5668c1e6dc827ff870f
Deivyson05 <deivysonrssantos@outlook.com>
2026-04-22 20:11:42 -0300

    feat: cria entidades User e Institution

---
+++ api/routes/institution.routes.ts
@@ -0,0 +1,10 @@
+
+import { Router } from "express";
+import { InstitutionController } from "../controllers/institution.controller.js";
+            
+const institutionRouter = Router();
+            
+//institutionRouter.post("/", InstitutionController.createUser);
+            
+export default institutionRouter;
+    
---
+++ api/services/institution.service.ts
@@ -0,0 +1,23 @@
+
+import { prisma } from "../../lib/prisma.js";
+import { HttpError } from "../core/httpError.js";
+
+export class InstitutionService {
+    static async create(body: any) {
+        //const { name, email, password } = body;
+
+        //if (!name || !email || !password) {
+        //    throw new HttpError("Name, email, and password are required", 400);
+        //}
+
+        //const institution = await prisma.institution.create({
+        //    data: {
+        //        
+        //    },
+        //    include: {
+        //        
+        //    },
+        //});
+    }
+}
+    
