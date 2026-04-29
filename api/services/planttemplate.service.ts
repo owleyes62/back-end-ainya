de42f22e689c350bc16b04e535a8218f905fff60
Deivyson05 <deivysonrssantos@outlook.com>
2026-04-29 20:48:55 -0300

    chore: adiciona tabela PlantTemplate

---
+++ api/services/planttemplate.service.ts
@@ -0,0 +1,23 @@
+
+import { prisma } from "../../lib/prisma.js";
+import { HttpError } from "../core/httpError.js";
+
+export class PlantTemplateService {
+    static async create(body: any) {
+        //const { name, email, password } = body;
+
+        //if (!name || !email || !password) {
+        //    throw new HttpError("Name, email, and password are required", 400);
+        //}
+
+        //const planttemplate = await prisma.planttemplate.create({
+        //    data: {
+        //        
+        //    },
+        //    include: {
+        //        
+        //    },
+        //});
+    }
+}
+    
