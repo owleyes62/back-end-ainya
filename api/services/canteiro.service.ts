6925599efd2d3e3dba46c613360ab2bf6f31ab3e
Deivyson05 <deivysonrssantos@outlook.com>
2026-04-29 20:54:01 -0300

    chore: adiciona tabela Canteiro

---
+++ api/services/canteiro.service.ts
@@ -0,0 +1,23 @@
+
+import { prisma } from "../../lib/prisma.js";
+import { HttpError } from "../core/httpError.js";
+
+export class CanteiroService {
+    static async create(body: any) {
+        //const { name, email, password } = body;
+
+        //if (!name || !email || !password) {
+        //    throw new HttpError("Name, email, and password are required", 400);
+        //}
+
+        //const canteiro = await prisma.canteiro.create({
+        //    data: {
+        //        
+        //    },
+        //    include: {
+        //        
+        //    },
+        //});
+    }
+}
+    
