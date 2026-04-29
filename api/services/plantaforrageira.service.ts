17adf3dae2f504c23860f106af23710de3de4a7b
Deivyson05 <deivysonrssantos@outlook.com>
2026-04-29 20:39:24 -0300

    chore: adiciona tabela PlantaForrageira

---
+++ api/services/plantaforrageira.service.ts
@@ -0,0 +1,23 @@
+
+import { prisma } from "../../lib/prisma.js";
+import { HttpError } from "../core/httpError.js";
+
+export class PlantaForrageiraService {
+    static async create(body: any) {
+        //const { name, email, password } = body;
+
+        //if (!name || !email || !password) {
+        //    throw new HttpError("Name, email, and password are required", 400);
+        //}
+
+        //const plantaforrageira = await prisma.plantaforrageira.create({
+        //    data: {
+        //        
+        //    },
+        //    include: {
+        //        
+        //    },
+        //});
+    }
+}
+    
