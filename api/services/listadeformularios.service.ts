08a12c8725972d106e9c7b632f325641b8f1539e
Deivyson05 <deivysonrssantos@outlook.com>
2026-04-29 20:59:56 -0300

    chore: adiciona tabela ListaDeFormularios

---
+++ api/services/listadeformularios.service.ts
@@ -0,0 +1,23 @@
+
+import { prisma } from "../../lib/prisma.js";
+import { HttpError } from "../core/httpError.js";
+
+export class ListaDeFormulariosService {
+    static async create(body: any) {
+        //const { name, email, password } = body;
+
+        //if (!name || !email || !password) {
+        //    throw new HttpError("Name, email, and password are required", 400);
+        //}
+
+        //const listadeformularios = await prisma.listadeformularios.create({
+        //    data: {
+        //        
+        //    },
+        //    include: {
+        //        
+        //    },
+        //});
+    }
+}
+    
