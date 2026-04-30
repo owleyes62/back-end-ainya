0a0d425a351662b01ccdb3d60bf7526129a63d68
Deivyson05 <deivysonrssantos@outlook.com>
2026-04-29 21:06:33 -0300

    chore: adiciona tabela Formulario

---
+++ api/routes/formulario.routes.ts
@@ -0,0 +1,10 @@
+
+import { Router } from "express";
+import { FormularioController } from "../controllers/formulario.controller.js";
+            
+const formularioRouter = Router();
+            
+//formularioRouter.post("/", FormularioController.createUser);
+            
+export default formularioRouter;
+    
