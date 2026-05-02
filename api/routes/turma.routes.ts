19eec510b0daa9d04b1707968e5695605c75b8a3
Deivyson05 <deivysonrssantos@outlook.com>
2026-05-02 17:42:37 -0300

    chore: adiciona tabela Turma

---
+++ api/routes/turma.routes.ts
@@ -0,0 +1,10 @@
+
+import { Router } from "express";
+import { TurmaController } from "../controllers/turma.controller.js";
+            
+const turmaRouter = Router();
+            
+//turmaRouter.post("/", TurmaController.createUser);
+            
+export default turmaRouter;
+    
