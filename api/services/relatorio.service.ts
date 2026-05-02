4016f5a11ee5317c69a2e09eaed870207f427e5a
Deivyson05 <deivysonrssantos@outlook.com>
2026-05-02 17:34:28 -0300

    chore: adiciona tabela Relatorio

---
+++ api/services/relatorio.service.ts
@@ -0,0 +1,23 @@
+
+import { prisma } from "../../lib/prisma.js";
+import { HttpError } from "../core/httpError.js";
+
+export class RelatorioService {
+    static async create(body: any) {
+        //const { name, email, password } = body;
+
+        //if (!name || !email || !password) {
+        //    throw new HttpError("Name, email, and password are required", 400);
+        //}
+
+        //const relatorio = await prisma.relatorio.create({
+        //    data: {
+        //        
+        //    },
+        //    include: {
+        //        
+        //    },
+        //});
+    }
+}
+    
