6694bbbd27ef00d6998d88824bc325a5cd4d158e
Deivyson05 <deivysonrssantos@outlook.com>
2026-05-02 17:26:16 -0300

    chore: adiciona tabela Photo

---
+++ api/routes/photo.routes.ts
@@ -0,0 +1,10 @@
+
+import { Router } from "express";
+import { PhotoController } from "../controllers/photo.controller.js";
+            
+const photoRouter = Router();
+            
+//photoRouter.post("/", PhotoController.createUser);
+            
+export default photoRouter;
+    
