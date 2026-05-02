5404ee6575c09816a4a0c7d5c3968b0d73c64420
Deivyson05 <deivysonrssantos@outlook.com>
2026-05-02 17:44:16 -0300

    chore: adiciona tabela AlunoTurma

---
+++ api/routes/alunoturma.routes.ts
@@ -0,0 +1,10 @@
+
+import { Router } from "express";
+import { AlunoTurmaController } from "../controllers/alunoturma.controller.js";
+            
+const alunoturmaRouter = Router();
+            
+//alunoturmaRouter.post("/", AlunoTurmaController.createUser);
+            
+export default alunoturmaRouter;
+    
