4ed6f9c2cf5d3aef8baa379c08d5f974e108ad74
Deivyson05 <deivysonrssantos@outlook.com>
2026-05-02 17:21:26 -0300

    chore: adiciona tabela Checklist

---
+++ api/routes/checklist.routes.ts
@@ -0,0 +1,10 @@
+
+import { Router } from "express";
+import { ChecklistController } from "../controllers/checklist.controller.js";
+            
+const checklistRouter = Router();
+            
+//checklistRouter.post("/", ChecklistController.createUser);
+            
+export default checklistRouter;
+    
---
+++ api/routes/measurement.routes.ts
@@ -0,0 +1,10 @@
+
+import { Router } from "express";
+import { MeasurementController } from "../controllers/measurement.controller.js";
+            
+const measurementRouter = Router();
+            
+//measurementRouter.post("/", MeasurementController.createUser);
+            
+export default measurementRouter;
+    
