531a14c715090ecb397a4a3457df2e9294f26434
Deivyson05 <deivysonrssantos@outlook.com>
2026-05-02 17:36:50 -0300

    chore: adiciona tabela AcademicPeriod

---
+++ api/routes/academicperiod.routes.ts
@@ -0,0 +1,10 @@
+
+import { Router } from "express";
+import { AcademicPeriodController } from "../controllers/academicperiod.controller.js";
+            
+const academicperiodRouter = Router();
+            
+//academicperiodRouter.post("/", AcademicPeriodController.createUser);
+            
+export default academicperiodRouter;
+    
