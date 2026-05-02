356b24d309a807051b38877bdcc5cef3cb6d840c
Deivyson05 <deivysonrssantos@outlook.com>
2026-05-02 17:21:26 -0300

    chore: adiciona tabela Checklist

---
+++ api/services/checklist.service.ts
@@ -0,0 +1,23 @@
+
+import { prisma } from "../../lib/prisma.js";
+import { HttpError } from "../core/httpError.js";
+
+export class ChecklistService {
+    static async create(body: any) {
+        //const { name, email, password } = body;
+
+        //if (!name || !email || !password) {
+        //    throw new HttpError("Name, email, and password are required", 400);
+        //}
+
+        //const checklist = await prisma.checklist.create({
+        //    data: {
+        //        
+        //    },
+        //    include: {
+        //        
+        //    },
+        //});
+    }
+}
+    
