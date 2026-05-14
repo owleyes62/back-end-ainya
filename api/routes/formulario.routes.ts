3773a4fc7fee0669c0e5510dc18a3b20792be705
Iwerson <109252776+owleyes62@users.noreply.github.com>
2026-05-14 22:10:38 +0000

    feat: rota de cheklist

--- api/routes/formulario.routes.ts
+++ api/routes/formulario.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { FormularioController } from "../controllers/formulario.controller.js";
+import { ChecklistController } from "../controllers/checklist.controller.js";
 
 const formularioRouter = Router();
 
-formularioRouter.get("/", FormularioController.findAllByUser);
+formularioRouter.post("/:id/checklist", ChecklistController.createManyForFormulario);
+formularioRouter.get("/:user_id", FormularioController.findAllByUser);
 formularioRouter.post("/", FormularioController.create);
 
 export default formularioRouter;
