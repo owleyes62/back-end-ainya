6062bd5dd7e6210a9a9b71e3a4e849f0ef0df8dd
Ingrid Beatriz Silva <108553706+ingrid-beatriiz@users.noreply.github.com>
2026-05-15 23:01:06 +0000

    feat: implementa rotas, controller e service para listagem, criacao e edicao de relatorios

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -3,6 +3,7 @@ import userRouter from "./user.routes.js";
 import canteiroRouter from "./canteiro.routes.js";
 import plantaforrageiraRouter from "./plantaforrageira.routes.js";
 import listadeformulariosRouter from "./listadeformularios.routes.js";
+import relatorioRouter from "./relatorio.routes.js"; 
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use("/users", userRouter);
 routes.use("/canteiros", canteiroRouter);
 routes.use("/plantas-forrageiras", plantaforrageiraRouter);
 routes.use("/listas-formularios", listadeformulariosRouter);
+routes.use("/relatorios", relatorioRouter);
 
 export default routes;
