270c0275dd7a7dfb4c1ba3a00d58d0f53082f226
Iwerson <109252776+owleyes62@users.noreply.github.com>
2026-05-24 09:52:24 -0300

    feat(routes): mount all new routers

--- api/routes/index.ts
+++ api/routes/index.ts
@@ -6,6 +6,16 @@ import canteiroRouter from "./canteiro.routes.js";
 import plantaforrageiraRouter from "./plantaforrageira.routes.js";
 import listadeformulariosRouter from "./listadeformularios.routes.js";
 import institutionRouter from "./institution.routes.js";
+import alunoRouter from "./aluno.routes.js";
+import userCanteiroRouter from "./usercanteiro.routes.js";
+import checklistRouter from "./checklist.routes.js";
+import measurementRouter from "./measurement.routes.js";
+import photoRouter from "./photo.routes.js";
+import relatorioRouter from "./relatorio.routes.js";
+import planttemplateRouter from "./planttemplate.routes.js";
+import turmaRouter from "./turma.routes.js";
+import alunoturmaRouter from "./alunoturma.routes.js";
+import academicperiodRouter from "./academicperiod.routes.js";
 
 const routes = Router();
 
@@ -24,5 +34,15 @@ routes.use("/formularios", formularioRouter);
 routes.use("/canteiros", canteiroRouter);
 routes.use("/plantas-forrageiras", plantaforrageiraRouter);
 routes.use("/listas-formularios", listadeformulariosRouter);
+routes.use("/alunos", alunoRouter);
+routes.use("/user-canteiros", userCanteiroRouter);
+routes.use("/checklist", checklistRouter);
+routes.use("/measurements", measurementRouter);
+routes.use("/photos", photoRouter);
+routes.use("/relatorios", relatorioRouter);
+routes.use("/plant-templates", planttemplateRouter);
+routes.use("/turmas", turmaRouter);
+routes.use("/aluno-turma", alunoturmaRouter);
+routes.use("/academic-periods", academicperiodRouter);
 
 export default routes;
